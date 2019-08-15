af18352c2caf394f6afa83860950bec9eadc42e6
kennbryll <53499702+kennbryll@users.noreply.github.com>
2019-08-15 09:56:26 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,12 +5,11 @@
 </head>
 <body>
 <center>
-<li>
 <h4> Enter your Email Address: <input type="text"> </h4>
 <h4> Confirm your Email Address: <input type="text"> </h4>
 <h4> Enter your Password: <input type="text"> </h4>
 <h4> Confirm your Password: <input type="text"> </h4>
-</li>
+  
 <form action="index.php">
 <input type="submit" value="Confirm">
 
