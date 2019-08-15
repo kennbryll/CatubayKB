26cf51d0873f64eb2f5c178486f2e0e14f6c3220
kennbryll <53499702+kennbryll@users.noreply.github.com>
2019-08-15 09:28:45 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Login</title>
+    
+</head>
+<body>
+    <header>
+        <div align="center">
+            <h2><b>Login</b></h2>
+        </div>
+    </header>
+    
+    <main>
+        <form>
+            <div align="center"><br>
+                <li><label for="username">Username:</label></li>
+                 <input type="text" name="username">
+            </div>
+            <div align="center">
+                <li><label>Password:</label></li>
+                <input type="password" name="password">
+            </div>
+	<br/>
+            <div align="center">
+                <input type="submit" value="Sign In">
+		<a href="index.php"Photo Gallery<a/>
+            </div
+	<br/>
+	    <div align="center">
+		<li><label>Reset Password</label></li>
+	<br/>
+	    <div align="center">
+		<li><label>Sign Up</label></li>
+         </form>
+    </main>
+    
+</body>
+</html>
