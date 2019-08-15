5e843ba54769dc29a30b4c375d0ff238ff8db9a5
kennbryll <53499702+kennbryll@users.noreply.github.com>
2019-08-15 09:48:08 +0800

    Create resetpassword.php

---
+++ resetpassword.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Reset Password </title>
+</head>
+<body>
+</br>
+</br>
+</br>
+</br>
+<h4> Please type your Email Address: <input type="text"> </h4>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
