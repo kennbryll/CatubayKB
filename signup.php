47c7856317bda796928e8cc35d2983a03fb7062f
kennbryll <53499702+kennbryll@users.noreply.github.com>
2019-08-15 09:55:16 +0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Sign Up </title>
+</head>
+<body>
+<center>
+<li>
+<h4> Enter your Email Address: <input type="text"> </h4>
+<h4> Confirm your Email Address: <input type="text"> </h4>
+<h4> Enter your Password: <input type="text"> </h4>
+<h4> Confirm your Password: <input type="text"> </h4>
+</li>
+<form action="index.php">
+<input type="submit" value="Confirm">
+
+
+
+
+
+
+</body>
+</html>
